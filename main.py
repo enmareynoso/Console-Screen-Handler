6edf7807d7978b0c6e94cec510182dc9bc4a187b
Enmanuel Reynoso <enmanueldev@gmail.com>
2022-12-06 21:51:34 -0400

    restructured main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import json 
+import json
 
-def capture_user_info():
-    name = input("Please enter your name: ")
-    last_name = input("Please enter your last name: ")
-    age = int(input("Please enter your age: "))
+def capture_user():
+    users = []
+
+    name = input("Please enter your name:")
+    last_name = input("Please enter your last name:")
+    age = int(input("Please enter your age:"))
 
     user_info = {
         "name": name,
@@ -11,30 +13,18 @@ def capture_user_info():
         "age": age
     }
 
-    return user_info
+    users.append(user_info)
 
-def store_user_info(user_info):
-    with open("user_info_json","r") as json_file:
-        data = json.load(json_file)
-        data.append(user_info)
-    with open("user_info_json","w") as json_file:
-        json.dump(data,json_file)  
-
-
-
-while True:
-     user_choice = input("Would you like to store your information in a JSON file (a), or exit the program(x): ")
-
-     if user_choice == "a":
-         user_info = capture_user_info()
-         store_user_info(user_info)
-     elif user_choice == "x":
-            break
-     else:
-      print("Invalid option. Enter a or x")
+    return users
 
     
+def main():
+
+    users = capture_user()
+
+    with open("user_info.json", "w") as file:
+        json.dump(users,file)
 
 
-    
-
+if __name__ == "__main__":
+    main()
